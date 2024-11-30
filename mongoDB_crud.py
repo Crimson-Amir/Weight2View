61234f5cf7969128da0b12196fcfd4f5dfa1c0cb
CrimsonAmir <ytchanneltinos@gmail.com>
2024-12-01 02:34:50 +0330

    add mongoDB crud file

---
+++ mongoDB_crud.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from setting import mongodb_atlas_url
+
+client = MongoClient(mongodb_atlas_url)
+weight2view_db = client.weight2view
+item_collection = weight2view_db.items
+
+def add_new_item_to_db(**kwargs):
+    """
+    item_name: str,
+    weight_in_gram: float,
+    height_mm: float,
+    width_mm: float,
+    length_mm: float
+    """
+    post_id = item_collection.insert_one(kwargs).inserted_id
+    return post_id
+
+def find_one_item(**kwargs):
+    return item_collection.find_one(kwargs)
+
+def find_items(condition:dict, includes_parameter:dict):
+    return item_collection.find(condition, includes_parameter)
+
+def search_in_items(search, **kwargs):
+    return item_collection.aggregate([
+        {
+            '$search': {
+                search
+            }
+        },
+        kwargs
+    ])
---
+++ setting.py
@@ -0,0 +1,2 @@
+mongodb_atlas_url = ('mongodb+srv://ytchanneltinos:amir1383amir@cluster1.rwd2p.mongodb.net/'
+                     '?retryWrites=true&w=majority&appName=Cluster1')
