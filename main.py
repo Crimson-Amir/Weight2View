5466b411bfb054b6659799e0757261ca5c68e543
CrimsonAmir <ytchanneltinos@gmail.com>
2024-12-01 22:15:24 +0330

    update fastapi section

--- main.py
+++ main.py
@@ -9,12 +9,36 @@ async def home():
 
 @app.post('/add_new_item_to_db')
 async def add_new_item_to_database(new_item: schema.AddNewItemSchema):
-    mongoDB_crud.add_new_item_to_db(new_item.dict())
+    try:
+        document_id = mongoDB_crud.add_new_item_to_db(**new_item.model_dump())
+        return {'status': 'OK', 'document_id': document_id}
+    except Exception as e:
+        return {'status': 'Error', 'error_type': type(e), 'error_reason': str(e)}
 
-@app.get('/find_first_item')
-async def find_first_item():
-    pass
+@app.post('/find_first_item')
+async def find_first_item(item_condition: schema.ItemCondition):
+    try:
+        cleand_data = {k: v for k, v in item_condition.model_dump().items() if v is not None}
+        item = mongoDB_crud.find_one_item(cleand_data)
+        if item:
+            item['_id'] = str(item['_id'])
+            return {'status': 'OK', 'item': dict(item)}
+        return {'status': 'NOK', 'detail': 'there is no item with this specification'}
+    except Exception as e:
+        return {'status': 'Error', 'error_type': type(e), 'error_reason': str(e)}
 
-@app.get('f/ind_items')
-async def find_items():
-    pass
+
+@app.post('/find_items')
+async def find_items(item_condition: schema.ItemCondition):
+    try:
+        cleand_data = {k: v for k, v in item_condition.model_dump().items() if v is not None}
+        items = mongoDB_crud.find_items(cleand_data, {})
+        if items:
+            items_list = []
+            for item in items:
+                item['_id'] = str(item['_id'])
+                items_list.append(item)
+            return {'status': 'OK', 'items': items_list}
+        return {'status': 'NOK', 'detail': 'there is no item with this specification'}
+    except Exception as e:
+        return {'status': 'Error', 'error_type': type(e), 'error_reason': str(e)}
--- schema.py
+++ schema.py
@@ -6,3 +6,11 @@ class AddNewItemSchema(BaseModel):
     height_mm: float
     width_mm: float
     length_mm: float
+
+class ItemCondition(BaseModel):
+    item_id: str | None = None
+    item_name: str | None = None
+    weight_in_gram: float | None = None
+    height_mm: float | None = None
+    width_mm: float | None = None
+    length_mm: float | None = None
