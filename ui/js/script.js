5cd88c411bbb37ae3ff5b029456673488b2ae173
Mohammad Hassan Saboury <mhasaboury@gmail.com>
2024-12-03 01:00:09 +0330

    First commit, initialize UI

---
+++ ui/js/script.js
@@ -0,0 +1,13 @@
+import '../style.css';
+import { createApp } from 'vue';
+
+  const app = {
+    data() {
+      return {
+        text: 'Weight2View'
+      }
+    },
+    methods: {}
+  };
+
+createApp(app).mount('#app');
---
+++ ui/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default {
+    plugins: [vue()],
+    resolve: {
+        alias: {
+          vue: 'vue/dist/vue.esm-bundler.js',
+        }
+    },
+    server: {
+        open: true
+    }
+}
